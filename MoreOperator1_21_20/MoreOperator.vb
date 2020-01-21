caf1447afde0d92489414f4298d82ce6de232f94
Jemika Smith <2cheerbabe3@gmail.com>
2020-01-21 08:56:57 -0700

    Changes

---
+++ MoreOperator1_21_20/MoreOperator.vb
@@ -0,0 +1,23 @@
+﻿Option Explicit On
+Option Strict On
+'Jemika Smith
+'RCET0265
+'Spring2020
+'More Operator
+Module MoreOperator
+
+    Sub Main()
+
+        'promt user for a message
+        'then display the message
+
+        Dim userMessage As String
+        'userMessage = "Hello"
+
+        Console.WriteLine("Please enter a message. Press enter when done")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You entered " & userMessage & "Good job!")
+        Console.ReadLine()
+    End Sub
+
+End Module
