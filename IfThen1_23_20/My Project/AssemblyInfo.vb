bb78327729e756a35562cd22c40bf2e50d766f46
Jemika Smith <2cheerbabe3@gmail.com>
2020-01-23 08:22:25 -0700

    Created simple IfThenElse Example

---
+++ IfThen1_23_20/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("IfThen1_23_20")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("IfThen1_23_20")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("db9b1815-57b5-48b3-8fac-3e287318c246")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ IfThen1_23_20/ifThen.vb
@@ -0,0 +1,19 @@
+﻿Option Explicit On
+Option Strict On
+'Jemika Smith
+'RCET0265
+'Spring2020
+'ifThen
+Module IfThen
+
+    Sub Main()
+
+        If 1 > 2 Then
+            Console.WriteLine("True")
+        Else
+            Console.WriteLine("False")
+        End If
+
+    End Sub
+
+End Module
