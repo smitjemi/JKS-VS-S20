745e81f7c2c391c6300aab6f29a9bce4e1c106ec
Jemika Smith <2cheerbabe3@gmail.com>
2020-01-27 08:45:46 -0700

    Wrote the Simple Calculator assignment

---
+++ SimpleCalculator/SimpleCalculator.vb
@@ -0,0 +1,30 @@
+﻿Option Explicit On
+Option Strict On
+'Jemika Smith
+'RCET0265
+'Spring2020
+'Simple Calculator
+Module SimpleCalculator
+
+    Sub Main()
+        'prompt user a message
+        'then display the message
+        Dim userMessage As String
+        Dim userMessage2 As String
+        Dim userMessage3 As String
+
+        Console.WriteLine(" Hello, can you give me a number?  ")
+        userMessage = Console.ReadLine()
+        Console.WriteLine(" Hello, can you give me another number? ")
+        userMessage2 = Console.ReadLine()
+        Console.WriteLine(" Do you want to add or multiply ")
+        userMessage3 = Console.ReadLine()
+
+        If userMessage3 = "Add" Or userMessage3 = "+" Then
+            Console.WriteLine(CInt(userMessage) + CInt(userMessage2))
+        ElseIf userMessage3 = "Multiply" Or userMessage3 = "*" Then
+            Console.WriteLine(CInt(userMessage) * CInt(userMessage2))
+        End If
+    End Sub
+
+End Module
