933fd55b769f99f2ca970287a4464e6dc2fc25ed
Jemika Smith <2cheerbabe3@gmail.com>
2020-01-28 08:39:26 -0700

    Created loop example

---
+++ LoopExample1-28-20/LoopExample.vb
@@ -0,0 +1,27 @@
+﻿Option Explicit On
+Option Strict On
+'Jemika Smith
+'RCET0256
+'Spring2020
+'LoopExample
+'https://github.com/smitjemi/JKS-VS-S20
+Module LoopExample
+
+    Sub Main()
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim result As Integer
+
+        firstNumber = 5
+        secondNumber = 7
+        result = firstNumber + secondNumber
+
+        For loopCount = 0 To 25 Step 5
+            result = result + 3
+            Console.WriteLine(loopCount)
+        Next
+        Console.WriteLine(result)
+        Console.ReadLine()
+    End Sub
+
+End Module
