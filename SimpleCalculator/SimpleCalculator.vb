20bd222c87423aa1b39bb90374b313a0a8b68af4
Jemika Smith <2cheerbabe3@gmail.com>
2020-01-28 08:19:24 -0700

    created the determine the user's choice

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -4,6 +4,7 @@ Option Strict On
 'RCET0265
 'Spring2020
 'Simple Calculator
+'https://github.com/smitjemi/JKS-VS-S20
 Module SimpleCalculator
 
     Sub Main()
@@ -13,17 +14,34 @@ Module SimpleCalculator
         Dim userMessage2 As String
         Dim userMessage3 As String
 
-        Console.WriteLine(" Hello, can you give me a number?  ")
+        Console.WriteLine("Hello, can you give me a number? ")
         userMessage = Console.ReadLine()
-        Console.WriteLine(" Hello, can you give me another number? ")
+        Console.WriteLine("Hello, can you give me another number?")
         userMessage2 = Console.ReadLine()
-        Console.WriteLine(" Do you want to add or multiply ")
+        Console.WriteLine("Do you want to add or multiply?")
         userMessage3 = Console.ReadLine()
 
         If userMessage3 = "Add" Or userMessage3 = "+" Then
             Console.WriteLine(CInt(userMessage) + CInt(userMessage2))
         ElseIf userMessage3 = "Multiply" Or userMessage3 = "*" Then
             Console.WriteLine(CInt(userMessage) * CInt(userMessage2))
+
+            'Determine the user's choice
+            If userMessage = "Add" Or userMessage = "+" Then
+                Try
+                    Console.WriteLine(CInt(userMessage) + CInt(userMessage2))
+                Catch ex As Exception
+                    'Console.WriteLine(ex.Message.ToString)
+                End Try
+            ElseIf userMessage = "Multiply" Or userMessage = "*" Then
+                Try
+                    Console.WriteLine(CInt(userMessage) * CInt(userMessage2))
+                Catch ex As Exception
+                    Console.WriteLine("ERROR, Please enter a whole number")
+                End Try
+            Else
+                Console.WriteLine("Invalid Selection")
+            End If
         End If
     End Sub
 
