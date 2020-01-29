c8021f2dc52b791a9bdb915366469c8cf50b4cfd
Jemika Smith <2cheerbabe3@gmail.com>
2020-01-29 08:56:33 -0700

    Added Loops

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -1,5 +1,6 @@
 ﻿Option Explicit On
 Option Strict On
+Option Compare Text
 'Jemika Smith
 'RCET0265
 'Spring2020
@@ -8,44 +9,77 @@ Option Strict On
 Module SimpleCalculator
 
     Sub Main()
-        'prompt user a message
-        'then display the message
-        Dim userMessage As String
-        Dim userMessage2 As String
+
+        Dim userMessage As Integer
+        Dim userMessage2 As Integer
         Dim userMessage3 As String
+        Dim quitProgram As Boolean
+        Dim promptUser As Boolean
 
-        Console.WriteLine("Hello, can you give me a number? ")
-        userMessage = Console.ReadLine()
-        Console.WriteLine("Hello, can you give me another number?")
-        userMessage2 = Console.ReadLine()
-        Console.WriteLine("Do you want to add or multiply?")
-        userMessage3 = Console.ReadLine()
+        quitProgram = False
+        promptUser = True
 
-        If userMessage3 = "Add" Or userMessage3 = "+" Then
-            Console.WriteLine(CInt(userMessage) + CInt(userMessage2))
-        ElseIf userMessage3 = "Multiply" Or userMessage3 = "*" Then
-            Console.WriteLine(CInt(userMessage) * CInt(userMessage2))
+        Do While quitProgram = False
 
-            'Determine the user's choice
-            If userMessage = "Add" Or userMessage = "+" Then
+            'prompt user for two numbers 
+            Console.WriteLine("Hello, can you give me a number? ")
+            Do While promptUser = True
                 Try
-                    Console.WriteLine(CInt(userMessage) + CInt(userMessage2))
+                    userMessage = CInt(Console.ReadLine())
+                    promptUser = False
                 Catch ex As Exception
-                    Console.WriteLine(ex.Message.ToString)
-                    Console.WriteLine(ex.ToString)
-                End Try
-            ElseIf userMessage = "Multiply" Or userMessage = "*" Then
-                Try
-                    Console.WriteLine(CInt(userMessage) * CInt(userMessage2))
-                Catch ex As InvalidCastException
                     Console.WriteLine("ERROR, Please enter a whole number")
-                Catch ex As Exception
-                    Console.WriteLine("You Broke it")
+                    promptUser = True
                 End Try
-            Else
-                Console.WriteLine("Invalid Selection")
-            End If
-        End If
+            Loop
+            Console.WriteLine("Hello, can you give me another number?")
+            Try
+                userMessage2 = CInt(Console.ReadLine())
+            Catch ex As Exception
+                Console.WriteLine("ERROR, Please enter a whole number")
+            End Try
+
+            'promt user for sum or product choice
+            'then display the message
+            Console.WriteLine("Do you want to add or multiply?")
+            userMessage3 = Console.ReadLine()
+
+            If userMessage3 = "Add" Or userMessage3 = "+" Then
+                    Console.WriteLine(userMessage + userMessage2)
+                ElseIf userMessage3 = "Multiply" Or userMessage3 = "*" Then
+                    Console.WriteLine(userMessage * userMessage2)
+
+                    'Determine the user's choice
+                    If userMessage3 = "Add" Or userMessage3 = "+" Then
+                        Try
+                            Console.WriteLine(userMessage + userMessage2)
+                        Catch ex As Exception
+                            Console.WriteLine("ERROR, Please enter a whole number")
+                            Console.WriteLine("YOu broke it")
+                        End Try
+                    ElseIf userMessage3 = "Multiply" Or userMessage3 = "*" Then
+                        Try
+                            Console.WriteLine(userMessage * userMessage2)
+                        Catch ex As InvalidCastException
+                            Console.WriteLine("ERROR, Please enter a whole number")
+                        Catch ex As Exception
+                            Console.WriteLine("You Broke it")
+                        End Try
+                    Else
+                        Console.WriteLine("Invalid Selection")
+                    End If
+                End If
+                'display result prompt to end
+                Console.WriteLine("Have a great day")
+                Console.WriteLine("Press Any To Run Again. Enter Q to quit.")
+
+                If Console.ReadLine() = "q" Then
+                    quitProgram = True
+                Else
+                    quitProgram = False
+                End If
+                Console.Clear()
+            Loop
     End Sub
 
 End Module
