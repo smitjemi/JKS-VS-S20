f6f82d81bb0782fe0b6ba385cca6bf6a9a8b82de
Jemika Smith <2cheerbabe3@gmail.com>
2020-01-30 08:57:31 -0700

    Started MySub

---
+++ SubFunctionExample/MySub.vb
@@ -0,0 +1,44 @@
+﻿'Jemika Smith
+'RCET0265
+'Spring2020
+'SubFunction
+Option Explicit On
+Option Strict On
+Module MySub
+
+    Sub Main()
+        Dim myNumber As Integer
+        myNumber = 7
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+
+        MySub(myNumber)
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+
+        myNumber = MyFunc(myNumber)
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        'Console.WriteLine("In Sub Main")
+        'Console.WriteLine(MyFunc(myNumber))
+
+        Console.ReadLine()
+    End Sub
+    Sub MySub(myNumber As Integer)
+        'Dim myNumber As Integer
+        myNumber += 2
+        Console.WriteLine("In Sub MySub")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+    End Sub
+    Function MyFunc(myNumber As Integer) As Integer
+        'Dim myNumber As Integer
+        myNumber -= 3
+        Console.WriteLine("In Function MyFunc")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        Return myNumber
+    End Function
+End Module
