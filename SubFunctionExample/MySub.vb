8bb88e692b5ad48c6452dfd8b91b8fa1ae0e25ee
Jemika Smith <2cheerbabe3@gmail.com>
2020-02-04 08:56:07 -0700

    Added doMath and ShowPattern

--- SubFunctionExample/MySub.vb
+++ SubFunctionExample/MySub.vb
@@ -2,33 +2,33 @@
 'RCET0265
 'Spring2020
 'SubFunction
+'https://github.com/smitjemi/JKS-VS-S20
 Option Explicit On
 Option Strict On
 Module MySub
 
     Sub Main()
-        Dim myNumber As Integer
-        myNumber = 7
-        Console.WriteLine("In Sub Main")
-        Console.WriteLine(myNumber)
-        Console.ReadLine()
-
-        MySub(myNumber)
-        Console.WriteLine("In Sub Main")
-        Console.WriteLine(myNumber)
-
-        myNumber = MyFunc(myNumber)
-        Console.WriteLine("In Sub Main")
-        Console.WriteLine(myNumber)
-        Console.ReadLine()
+        'Dim myNumber As Integer
+        'myNumber = 7
         'Console.WriteLine("In Sub Main")
-        'Console.WriteLine(MyFunc(myNumber))
+        'Console.WriteLine(myNumber)
+        'Console.ReadLine()
 
+        'MySub(myNumber)
+        'Console.WriteLine("In Sub Main")
+        'Console.WriteLine(myNumber)
+
+        'myNumber = MyFunc(myNumber)
+        'Console.WriteLine("In Sub Main")
+        'Console.WriteLine(myNumber)
+
+        'DoMath(7, 5, "*")
+        ShowPattern(5)
         Console.ReadLine()
     End Sub
     Sub MySub(myNumber As Integer)
         'Dim myNumber As Integer
-        myNumber += 2
+        'myNumber += 2
         Console.WriteLine("In Sub MySub")
         Console.WriteLine(myNumber)
         Console.ReadLine()
@@ -41,4 +41,28 @@ Module MySub
         Console.ReadLine()
         Return myNumber
     End Function
+    Sub DoMath(firstNumber As Integer, secondNumber As Integer, operatorChoice As String)
+        Dim result As Double
+        If operatorChoice = "+" Then
+            result = (firstNumber + secondNumber)
+        ElseIf operatorChoice = "-" Then
+            result = (firstNumber - secondNumber)
+        ElseIf operatorChoice = "/" Then
+            result = (firstNumber / secondNumber)
+        ElseIf operatorChoice = "*" Then
+            result = (firstNumber * secondNumber)
+        End If
+        Console.WriteLine(result)
+    End Sub
+    Sub ShowPattern(length As Integer)
+        Dim character As String
+        Dim pattern As String
+        character = "*"
+        For index = 1 To length
+            pattern &= character
+            Console.WriteLine(pattern)
+        Next
+
+    End Sub
+
 End Module
