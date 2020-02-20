11280f79e03f2c34b6add6c9527898fa9f0929c7
Jemika Smith <2cheerbabe3@gmail.com>
2020-02-20 08:44:20 -0700

    Created list example

---
+++ ListExample2_20_20/ListExample.vb
@@ -0,0 +1,31 @@
+﻿'Jemika Smith
+'RCET0265
+'Spring2020
+'List Example
+'https://github.com/smitjemi/JKS-VS-S20
+Option Explicit On
+Option Strict On
+Module ListExample
+
+    Sub Main()
+        Dim names As New List(Of String)
+        Dim searchString As String
+
+        names.Add("Tim")
+        names.Add("Joe")
+        'names.Add(Console.ReadLine())
+
+        searchString = Console.ReadLine()
+        If names.Contains(searchString) = True Then
+            Console.WriteLine("Yes, " & searchString & " is at index" & names.IndexOf("searchString"))
+        Else
+            Console.WriteLine("Sorry, " & searchString & " was not found.")
+        End If
+
+        'For Each name In names
+        '    Console.WriteLine(name)
+        'Next
+        Console.ReadLine()
+    End Sub
+
+End Module
