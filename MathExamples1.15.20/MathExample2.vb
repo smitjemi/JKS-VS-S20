0bb3dec6d8157bd10c578704406ae24a636d16e3
Jemika Smith <2cheerbabe3@gmail.com>
2020-02-24 08:11:42 -0700

    tryfixstuff

---
+++ MathExamples1.15.20/MathExample2.vb
@@ -0,0 +1,47 @@
+﻿'Jemika Smith
+'RCET0265
+'Spring 2020
+'Math Example
+
+
+Module Module1
+
+    Sub Main()
+
+        'Addition Operator
+        Console.WriteLine("2 + 2 = ")
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+
+        'Subtraction Operator
+        Console.WriteLine("4 - 2 = ")
+        Console.WriteLine(4 - 2)
+        Console.ReadLine()
+
+        'Multiply Operator
+        Console.WriteLine("2 * 2 = ")
+        Console.WriteLine(2 * 2)
+        Console.ReadLine()
+
+        'Division Operator. Decimal 
+        Console.WriteLine("25 / 3 = ")
+        Console.WriteLine(25 / 3)
+        Console.ReadLine()
+
+        'Integer Division Operator. Whole Number
+        Console.WriteLine("25 \ 3 = ")
+        Console.WriteLine(25 \ 3)
+        Console.ReadLine()
+
+        'Modulus Operator. Remainder
+        Console.WriteLine("25 Mod 3 = ")
+        Console.WriteLine(25 Mod 3)
+        Console.ReadLine()
+
+        'Exponant
+        Console.WriteLine("2^8 = ")
+        Console.WriteLine(2 ^ 8)
+        Console.ReadLine()
+    End Sub
+
+End Module
