b5696645220fdf70db553df6ca0c82383b6e75eb
Jemika Smith <2cheerbabe3@gmail.com>
2020-02-26 08:52:17 -0700

    Created Assignment

--- AccumulateMessage2_24_20/AccumulateMessage.vb
+++ AccumulateMessage2_24_20/AccumulateMessage.vb
@@ -4,5 +4,16 @@
 'Accumutlate Message
 'https://github.com/smitjemi/JKS-VS-S20
 Public Class AccumulateMessage
-
+    Sub Main()
+        'MsgBox(UserMessages(True, "Hello", False)
+        Console.WriteLine(UserMessage(True, "Hello 1", False))
+        Console.WriteLine(UserMessage(True, "Hello 2", False))
+        Console.WriteLine(UserMessage(True, "Hello 3", False))
+        MsgBox(UserMessages(True, "", False))
+    End Sub
+    Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
+        Dim formattedMessages As String = ""
+        formattedMessages &= message
+        Return formattedMessages
+    End Function
 End Class
