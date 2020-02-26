5e0eb3533aff15543d788eb9fb0d3930642f4462
Jemika Smith <2cheerbabe3@gmail.com>
2020-02-26 08:54:40 -0700

    fixed usermessages

--- AccumulateMessage2_24_20/AccumulateMessage.vb
+++ AccumulateMessage2_24_20/AccumulateMessage.vb
@@ -6,14 +6,14 @@
 Public Class AccumulateMessage
     Sub Main()
         'MsgBox(UserMessages(True, "Hello", False)
-        Console.WriteLine(UserMessage(True, "Hello 1", False))
-        Console.WriteLine(UserMessage(True, "Hello 2", False))
-        Console.WriteLine(UserMessage(True, "Hello 3", False))
+        Console.WriteLine(UserMessages(True, "Hello 1", False))
+        Console.WriteLine(UserMessages(True, "Hello 2", False))
+        Console.WriteLine(UserMessages(True, "Hello 3", False))
         MsgBox(UserMessages(True, "", False))
     End Sub
     Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
-        Dim formattedMessages As String = ""
-        formattedMessages &= message
+        Static formattedMessages As String '= ""
+        formattedMessages &= message & vbNewLine
         Return formattedMessages
     End Function
 End Class
