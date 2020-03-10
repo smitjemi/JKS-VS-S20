fedf086a879730dbef2ee4e13e97237452e00b6f
Jemika Smith <2cheerbabe3@gmail.com>
2020-03-10 08:57:53 -0600

    Continued

--- Roll Of The Dice3_9_20/RollofTheDice.vb
+++ Roll Of The Dice3_9_20/RollofTheDice.vb
@@ -5,28 +5,56 @@
 Module RollofTheDice
 
     Sub Main()
-        Console.WriteLine(getFirstRandomInteger1To6)
-        Console.WriteLine(getSecondRandomInteger1To6)
-
+        Dim DiceSum As Single
+        Console.WriteLine(FirstDice)
+        Console.WriteLine(SecondDice)
+        Console.WriteLine(DiceTotal)
+        Totals(2)
     End Sub
-    Private Function getFirstRandomInteger1To6() As Integer
-        Dim maxSingle As Single : maxSingle = 5
+    Private Function FirstDice() As Integer
+        Dim maxSingle As Single : maxSingle = 6
         Dim minSingle As Single : minSingle = 1
         Dim tempSingle As Single
-        Randomize(System.DateTime.Now.Millisecond) 'set seed to current system millisecond
-        tempSingle = ((maxSingle - minSingle + 1) * Rnd() + minSingle) 'actually get the random number in the range
-        getFirstRandomInteger1To6 = Convert.ToInt32(tempSingle) 'return the number
-        Console.WriteLine(getFirstRandomInteger1To6)
+        Randomize(System.DateTime.Now.Millisecond)
+        tempSingle = ((maxSingle - minSingle + 1) * Rnd() + maxSingle)
+        FirstDice = Convert.ToInt32(tempSingle)
+        Console.WriteLine("FirstDi =")
+        Console.WriteLine(FirstDice)
         Console.ReadLine()
     End Function
-    Private Function getSecondRandomInteger1To6() As Integer
-        Dim maxSingle As Single : maxSingle = 5
+    Private Function SecondDice() As Integer
+        Dim maxSingle As Single : maxSingle = 6
         Dim minSingle As Single : minSingle = 1
         Dim tempSingle As Single
-        Randomize(System.DateTime.Now.Millisecond) 'set seed to current system millisecond
-        tempSingle = ((maxSingle - minSingle + 1) * Rnd() + minSingle) 'actually get the random number in the range
-        getSecondRandomInteger1To6 = Convert.ToInt32(tempSingle) 'return the number
-        Console.WriteLine(getSecondRandomInteger1To6)
+        Randomize(System.DateTime.Now.Millisecond)
+        tempSingle = ((maxSingle - minSingle + 1) * Rnd() + maxSingle)
+        SecondDice = Convert.ToInt32(tempSingle)
+        Console.WriteLine("SecondDi =")
+        Console.WriteLine(SecondDice)
         Console.ReadLine()
+    End Function
+    Private Function DiceTotal() As Integer
+        Dim DiceSum As Single
+        DiceSum = FirstDice() + SecondDice()
+        Console.WriteLine("FirstDi + SecondDi = " & DiceSum)
+    End Function
+    Private Function Totals(currentNumber As Integer) As Integer
+        Static myArray(currentNumber) As Integer
+        myArray(currentNumber) = 12
+        myArray = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+        Static numbers(12) As Integer
+        ReDim Preserve myArray(12)
+        Console.ReadLine()
+        'Static myArray(currentNumber) As Integer
+
+        'myArray(1) = currentNumber
+
+        'myArray = {1, 2, 3, 4}
+
+        'myArray(0) = 5
+        'ReDim Preserve myArray(15)
+
+
+        'Console.ReadLine()
     End Function
 End Module
