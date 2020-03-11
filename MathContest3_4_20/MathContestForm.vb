765852c78e7fba754596c6af07ec01a33388b2d1
Jemika Smith <2cheerbabe3@gmail.com>
2020-03-10 20:47:29 -0600

    changes

--- MathContest3_4_20/MathContestForm.vb
+++ MathContest3_4_20/MathContestForm.vb
@@ -4,43 +4,38 @@
 'Math Contest
 'https://github.com/smitjemi/JKS-VS-S20
 Public Class MathContestForm
+    Dim userMessage As String
+    Dim firstNumberGood As Boolean
+    Dim secNumberGood As Boolean
+    Dim studentAnswerGood As Boolean
+    Dim randomNumber As Integer
+    Dim studentAnswer As Integer
+    Dim correctAnswer As Integer
+    Dim firstNumber As Integer
+    Dim secondNumber As Integer
     Private Sub MathContestForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim userMessage As String = ""
-        Dim firstNumberGood As Boolean
-        Dim secNumberGood As Boolean
-        Dim studentAnswerGood As Boolean
         firstNumberGood = False
         secNumberGood = False
         studentAnswerGood = False
-        Dim randomNumber As Integer
         'Random Number Generator
         randomNumber = CInt(Int((4 * Rnd()) + 0))
         firstNumberTextBox.Text = Str(Int((20 * Rnd()) + 0))
         secondNumberTextBox.Text = Str(Int((20 * Rnd()) + 0))
     End Sub
     Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
-        Dim userMessage As String
-        Dim NameTextBox As String
-        'Dim firstNumber As Integer
-        'Dim secondNumber As Integer
-        Dim studentAnswer As Integer
-        Dim correctAnswer As Integer
-        Dim randomNumber As Integer
-        Dim numberOfProblems As Integer = 0
-        Dim numbersCorrect As Integer = 0
-        randomNumber = CInt(Int((4 * Rnd()) + 0))
-        firstNumberTextBox.Text = Str(Int((20 * Rnd()) + 0))
-        secondNumberTextBox.Text = Str(Int((20 * Rnd()) + 0))
+        Dim numberOfProblems As Integer
+        Dim numbersCorrect As Integer
 
         If studentAnswer = correctAnswer Then
-            userMessage = "Good job " & NameTextBox & ", that is correct!"
-            numberOfProblems += 1
+            userMessage = "Good job, that is correct!"
+            numbersCorrect += 1
         Else
-            userMessage = "Sorry, " & NameTextBox & ", that is not correct. The correct " & "answer was " & correctAnswer & "."
+            userMessage = "Sorry, that is not correct. The correct answer was " & correctAnswer & "."
         End If
+        randomNumber = CInt(Int((20 * Rnd()) + 0))
+        firstNumberTextBox.Text = Str(Int((20 * Rnd()) + 0))
+        secondNumberTextBox.Text = Str(Int((20 * Rnd()) + 0))
         MsgBox(userMessage)
-        firstNumberTextBox.Text = ""
-        secondNumberTextBox.Text = ""
         studentAnswerTextBox.Text = ""
         SummaryButton.Enabled = True
         numberOfProblems += 1
@@ -48,7 +43,7 @@ Public Class MathContestForm
     Private Sub SummaryButton_Click(sender As Object, e As EventArgs) Handles SummaryButton.Click
         Dim numbersCorrect As Integer
         Dim numberOfProblems As Integer
-        MsgBox("You got " & numbersCorrect & " answers out of " & numberOfProblems & "correct.")
+        MsgBox("You got " & numbersCorrect & " answers correct out of " & numberOfProblems & " problems.")
     End Sub
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         Dim numbersCorrect As Integer = 0
@@ -68,6 +63,4 @@ Public Class MathContestForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
-
-
 End Class
