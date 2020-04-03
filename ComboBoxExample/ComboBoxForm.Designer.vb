35a1f37f0e303ff442d157ed843a24e20da98856
Jemika Smith <2cheerbabe3@gmail.com>
2020-04-03 08:51:27 -0600

    Started ComboBox Example

---
+++ ComboBoxExample/ComboBoxForm.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ComboBoxForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MainComboBox = New System.Windows.Forms.ComboBox()
+        Me.GoButton = New System.Windows.Forms.Button()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.AgeTextBox = New System.Windows.Forms.TextBox()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.AgeLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'MainComboBox
+        '
+        Me.MainComboBox.FormattingEnabled = True
+        Me.MainComboBox.Location = New System.Drawing.Point(245, 35)
+        Me.MainComboBox.Name = "MainComboBox"
+        Me.MainComboBox.Size = New System.Drawing.Size(293, 21)
+        Me.MainComboBox.TabIndex = 0
+        '
+        'GoButton
+        '
+        Me.GoButton.Location = New System.Drawing.Point(12, 342)
+        Me.GoButton.Name = "GoButton"
+        Me.GoButton.Size = New System.Drawing.Size(153, 96)
+        Me.GoButton.TabIndex = 1
+        Me.GoButton.Text = "Go"
+        Me.GoButton.UseVisualStyleBackColor = True
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.Location = New System.Drawing.Point(64, 36)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(153, 20)
+        Me.NameTextBox.TabIndex = 2
+        '
+        'AgeTextBox
+        '
+        Me.AgeTextBox.Location = New System.Drawing.Point(64, 62)
+        Me.AgeTextBox.Name = "AgeTextBox"
+        Me.AgeTextBox.Size = New System.Drawing.Size(153, 20)
+        Me.AgeTextBox.TabIndex = 3
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NameLabel.Location = New System.Drawing.Point(3, 36)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(51, 20)
+        Me.NameLabel.TabIndex = 4
+        Me.NameLabel.Text = "Name"
+        '
+        'AgeLabel
+        '
+        Me.AgeLabel.AutoSize = True
+        Me.AgeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AgeLabel.Location = New System.Drawing.Point(3, 62)
+        Me.AgeLabel.Name = "AgeLabel"
+        Me.AgeLabel.Size = New System.Drawing.Size(38, 20)
+        Me.AgeLabel.TabIndex = 5
+        Me.AgeLabel.Text = "Age"
+        '
+        'ComboBoxForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(576, 450)
+        Me.Controls.Add(Me.AgeLabel)
+        Me.Controls.Add(Me.NameLabel)
+        Me.Controls.Add(Me.AgeTextBox)
+        Me.Controls.Add(Me.NameTextBox)
+        Me.Controls.Add(Me.GoButton)
+        Me.Controls.Add(Me.MainComboBox)
+        Me.Name = "ComboBoxForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Combo Box"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MainComboBox As ComboBox
+    Friend WithEvents GoButton As Button
+    Friend WithEvents NameTextBox As TextBox
+    Friend WithEvents AgeTextBox As TextBox
+    Friend WithEvents NameLabel As Label
+    Friend WithEvents AgeLabel As Label
+End Class
---
+++ ComboBoxExample/ComboBoxForm.vb
@@ -0,0 +1,27 @@
+﻿'Jemika Smith
+'RCET0265
+'Spring 2020
+'ComboBox Example
+
+Public Class ComboBoxForm
+    'Determines how many users, 10 user Names and Ages.
+    Dim userInfo(9, 1) As String
+    Private Sub GoButton_Click(sender As Object, e As EventArgs) Handles GoButton.Click
+        Dim userIndex As Integer = 0
+        Const NAMEFIELDINDEX = 0I
+        Const AGEFIELDINDEX = 1I
+        'Load Name into the ComboBox
+        MainComboBox.Items.Add(NameTextBox.Text)
+        'Load Name and Age into array
+        userIndex = CInt(MainComboBox.Items.Count)
+        userInfo(userIndex, NAMEFIELDINDEX) = NameTextBox.Text
+        userInfo(userIndex, AGEFIELDINDEX) = AgeTextBox.Text
+        AgeTextBox.Text = MainComboBox.Items.Count
+        NameTextBox.Text = ""
+        AgeTextBox.Text = ""
+    End Sub
+    Private Sub MainComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles MainComboBox.SelectedIndexChanged
+        Me.Text = MainComboBox.SelectedIndex
+        NameTextBox.Text = MainComboBox.SelectedItem.ToString
+    End Sub
+End Class
