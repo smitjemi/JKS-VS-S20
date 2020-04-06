3a2d226391bbfdea0faf61da309e95f6af619fce
Jemika Smith <2cheerbabe3@gmail.com>
2020-04-05 19:01:30 -0600

    Added summary

--- Car Rental3_27_20/CarRentalForm.vb
+++ Car Rental3_27_20/CarRentalForm.vb
@@ -4,7 +4,7 @@
 'Car Rental Program
 'https://github.com/smitjemi/JKS-VS-S20
 Public Class CarRentalForm
-    Dim totalCustomer As Integer
+    Dim totalCustomers As Integer
     Dim totalDistance As Decimal
     Dim totalCharges As Decimal
     Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -19,6 +19,9 @@ Public Class CarRentalForm
         Dim end1 As Boolean = False
         Dim numberofDays As Integer
         Dim numberDays As Boolean = False
+        Dim mileCharge As Decimal
+        Dim miles As Decimal
+        Dim dailyCharge As Decimal
         If NameTextBox.Text = "" Then
             userMessage = "Please enter a Name" & vbNewLine
         End If
@@ -48,13 +51,13 @@ Public Class CarRentalForm
                 begOd = CDec(BegOdTextBox.Text)
                 beg1 = True
             Catch ex As Exception
-                userMessage &= "Please enter a Valid Beginning Number"
+                userMessage &= "Please enter a Valid Beginning Number" & vbNewLine
             End Try
             Try
                 endOd = CDec(EndOdTextBox.Text)
                 end1 = True
             Catch ex As Exception
-                userMessage &= "Please enter a Valid Ending Number"
+                userMessage &= "Please enter a Valid Ending Number" & vbNewLine
             End Try
             Try
                 numberofDays = CInt(DaysTextBox.Text)
@@ -86,77 +89,55 @@ Public Class CarRentalForm
                     End If
                 End If
             End If
-            If NameTextBox.Text = "" Then
-                NameTextBox.Select()
-            End If
-            If AddressTextBox.Text = "" Then
-                AddressTextBox.Select()
-            End If
-            If CityTextBox.Text = "" Then
-                CityTextBox.Select()
-            End If
-            If StateTextBox.Text = "" Then
-                StateTextBox.Select()
-            End If
-            If ZipTextBox.Text = "" Then
-                ZipTextBox.Select()
-            End If
-            If beg1 = False Then
-                BegOdTextBox.Select()
+            If DaysTextBox.Text = "" Then
+                DaysTextBox.Select()
             End If
             If end1 = False Then
                 EndOdTextBox.Select()
             End If
-            If DaysTextBox.Text = "" Then
-                DaysTextBox.Select()
+            If beg1 = False Then
+                BegOdTextBox.Select()
+            End If
+            If ZipTextBox.Text = "" Then
+                ZipTextBox.Select()
+            End If
+            If StateTextBox.Text = "" Then
+                StateTextBox.Select()
+            End If
+            If CityTextBox.Text = "" Then
+                CityTextBox.Select()
+            End If
+            If AddressTextBox.Text = "" Then
+                AddressTextBox.Select()
+            End If
+            If NameTextBox.Text = "" Then
+                NameTextBox.Select()
             End If
             ValidateCheckBox = True
         End If
-        'If ValidateAll() Then
-        '    'ToDo proceed to calculations and updates 
-        '    Console.WriteLine("Yes")
-        'Else
-        '    'ToDo alert user
-        '    Console.WriteLine("Nope")
-        '    'MsgBox(UserMessages(False, "", False))
-        'End If
+
         ''DailyCharge()
-        Const AAARATE = 0.05D
-        Const SENIORRATE = 0.03D
+        dailyCharge = 15
+        If MilesRadioButton.Checked = True Then
+            Select Case miles
+                Case <= 200 'First 200 miles driven are always free
+                    mileCharge = miles * 0
+                Case > 500 'Miles greater than 500 charged at .10 cents per mile.
+                    mileCharge += (miles - 500) * 0.1D + 36
+                Case Else 'Miles after free 200 is charged at .12 cents per mile.
+                    mileCharge = (miles - 200) * 0.12D
+            End Select
+        ElseIf KilometersRadioButton.Checked = True Then
+            miles = miles * 1.609D
+        End If
         Dim totalDiscount As Decimal = 0
         If AAACheckBox.Checked = True Then
-            totalDiscount = totalCharges * AAARATE
+            totalDiscount = totalCharges * 0.05D
         End If
         If SeniorCheckBox.Checked = True Then
-            totalDiscount += totalCharges * SENIORRATE
+            totalDiscount += totalCharges * 0.03D
         End If
-
-        'Return totalDiscount
-        ''Discont()
-        ''Console.WriteLine(MileageCharge(600))
-        'MileageCharge(220)
-        ''Check and Convert to Kilomoters
-
     End Sub
-    Function MileageCharge(ByRef miles As Decimal) As Decimal
-        'Mileage Charge
-        Dim mileCharge As Decimal
-        Const RATE_REGULAR = 0.12D
-        Const RATE_LOW = 0.1D
-        Const RATE_FREE = 0D
-        'All miles between 201 and 500 inclusive are .12 cents per mile.
-        'Miles greater than 500 charged at .10 cents per mile.
-        Select Case miles
-            Case <= 200 'First 200 miles driven are always free
-                mileCharge = miles * RATE_FREE
-            Case > 500
-                mileCharge = 300 * RATE_REGULAR
-                mileCharge += (miles - 500) * RATE_LOW
-            Case Else
-                mileCharge = (miles - 200) * RATE_REGULAR
-        End Select
-        Return mileCharge
-    End Function
     Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
         Static formattedMessages As String
         If clearMessage = True Then
@@ -184,6 +165,10 @@ Public Class CarRentalForm
 
     Private Sub SummaryButton_Click(sender As Object, e As EventArgs) Handles SummaryButton.Click
         MessageBox.Show("total message", "Detailed Summary")
+        totalCustomers += CInt(NameTextBox.Text)
+        totalDistance += CDec(MilesDrTextBox.Text)
+        totalCharges += CDec(OweTextBox.Text)
+
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
