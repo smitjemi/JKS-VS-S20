103f508f1693a004e24a09f200743f4ed3507caa
Jemika Smith <2cheerbabe3@gmail.com>
2020-04-07 12:28:36 -0600

    Final Code

--- Car Rental3_27_20/CarRentalForm.vb
+++ Car Rental3_27_20/CarRentalForm.vb
@@ -1,4 +1,6 @@
-﻿'Jemika Smith
+﻿Option Explicit On
+Option Strict On
+'Jemika Smith
 'RCET0265
 'Spring2020
 'Car Rental Program
@@ -12,7 +14,7 @@ Public Class CarRentalForm
     End Sub
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
         Dim ValidateCheckBox As Boolean = False
-        Dim userMessage As String = ""
+        Dim userMessage As String = "" 'Use a single message box to display any improper input
         Dim begOd As Decimal
         Dim beg1 As Boolean = False
         Dim endOd As Decimal
@@ -23,6 +25,7 @@ Public Class CarRentalForm
         Dim miles As Decimal
         Dim mileageTotal As Decimal
         Dim YouOwe As Decimal
+        'Include input validation for all text boxes
         If NameTextBox.Text = "" Then
             userMessage = "Please enter a Name" & vbNewLine
         End If
@@ -47,22 +50,23 @@ Public Class CarRentalForm
         If DaysTextBox.Text = "" Then
             userMessage &= "Please enter a Number of Days" & vbNewLine
         End If
+
         If BegOdTextBox.Text <> "" Or EndOdTextBox.Text <> "" Or DaysTextBox.Text <> "" Then
-            Try
+            Try 'Validate the odometer readings
                 begOd = CDec(BegOdTextBox.Text)
                 beg1 = True
             Catch ex As Exception
                 userMessage &= "Please enter a Valid Beginning Number" & vbNewLine
             End Try
-            Try
+            Try 'Validate the odometer readings
                 endOd = CDec(EndOdTextBox.Text)
                 end1 = True
             Catch ex As Exception
                 userMessage &= "Please enter a Valid Ending Number" & vbNewLine
             End Try
-            Try
+            Try 'Validate number of days
                 numberofDays = CInt(DaysTextBox.Text)
-                If numberofDays > 0 And numberofDays < 46 Then
+                If numberofDays > 0 And numberofDays < 46 Then 'Days must be greater than zero.Days cannot be greater than 45.
                     'GOOD!
                     numberDays = True
                 Else
@@ -79,6 +83,7 @@ Public Class CarRentalForm
                 End If
             End If
         End If
+        'Remove the invalid data from any affected text box
         If userMessage <> "" Then
             MsgBox(userMessage)
             If beg1 = False Then
@@ -93,6 +98,7 @@ Public Class CarRentalForm
         Else
             ValidateCheckBox = True
         End If
+        'Set the focus to the first affected text box in the tab order
         If numberofDays = False Then
             DaysTextBox.Select()
         End If
@@ -117,13 +123,14 @@ Public Class CarRentalForm
         If NameTextBox.Text = "" Then
             NameTextBox.Select()
         End If
+        'Do not display the message box until the calculate button is clicked
         If ValidateCheckBox = True Then
             daysCharged = (numberofDays * 15D) 'Daily Charge $15 per day
-            DayCharTextBox.Text = daysCharged.ToString("C")
-            If MilesRadioButton.Checked = True Then
+            DayCharTextBox.Text = daysCharged.ToString("C") 'Customer’s total daily charge format as currency
+            If MilesRadioButton.Checked = True Then 'The radio buttons determine if odometer value is miles/kilo
                 miles = endOd - begOd
-            Else
-                miles = (endOd - begOd) * 0.62D
+            Else '1 Kilometer equals .62 Miles.
+                miles = (endOd - begOd) * 0.62D 'convert kilo to miles
             End If
             MilesDrTextBox.Text = miles.ToString
             Select Case miles
@@ -134,7 +141,7 @@ Public Class CarRentalForm
                 Case Else 'Miles after free 200 is charged at .12 cents per mile.
                     mileageTotal = (miles - 200D) * 0.12D
             End Select
-            MileageTextBox.Text = mileageTotal.ToString("C")
+            MileageTextBox.Text = mileageTotal.ToString("C") 'Customer’s total mileage charge format as currency
 
             Dim totalCharges As Decimal
             totalCharges = CDec(DayCharTextBox.Text) + CDec(MileageTextBox.Text)
@@ -147,10 +154,12 @@ Public Class CarRentalForm
                 totalDiscount += totalCharges * 0.03D
             End If
             MinusTextBox.Text = totalDiscount
-            MinusTextBox.Text = totalDiscount.ToString("C")
+            MinusTextBox.Text = totalDiscount.ToString("C") 'Customer’s total discount applied format as currency
+
 
             YouOwe = CDec(totalCharges - totalDiscount)
-            OweTextBox.Text = YouOwe.ToString("C")
+            OweTextBox.Text = YouOwe.ToString("C") 'Customer’s total charges format as currency
+
             SummaryButton.Enabled = True
 
             totalCustomers += 1 'Total number of customers for the day
@@ -159,7 +168,7 @@ Public Class CarRentalForm
         End If
     End Sub
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
-        'Clear User Input
+        'Clear all input text boxes
         NameTextBox.Text = ""
         AddressTextBox.Text = ""
         CityTextBox.Text = ""
@@ -168,28 +177,32 @@ Public Class CarRentalForm
         BegOdTextBox.Text = ""
         EndOdTextBox.Text = ""
         DaysTextBox.Text = ""
+        'Clear all output text boxes
         MilesDrTextBox.Text = ""
         DayCharTextBox.Text = ""
         MileageTextBox.Text = ""
         MinusTextBox.Text = ""
         OweTextBox.Text = ""
+        'Clear discount check boxes
         AAACheckBox.Checked = False
         SeniorCheckBox.Checked = False
+        'Select the miles radio button
         MilesRadioButton.Select()
-        'Clear Output
+        'Do not clear the summary totals	
     End Sub
     Private Sub SummaryButton_Click(sender As Object, e As EventArgs) Handles SummaryButton.Click
         Dim stringVariable As String
-        stringVariable = "Total Customers:      " & totalCustomers.ToString & vbNewLine &
-            "Total Distance:         " & totalDistance.ToString & vbNewLine &
-            "Total Charge:            " & totalCharge.ToString("C")
-        MessageBox.Show(stringVariable, "Detailed Summary")
+        stringVariable = "Total Customers:      " & totalCustomers.ToString & vbNewLine & 'Total number of customers
+            "Total Distance:         " & totalDistance.ToString & vbNewLine & 'Total distance driven in miles
+            "Total Charge:            " & totalCharge.ToString("C") 'Total charges
+        MessageBox.Show(stringVariable, "Detailed Summary") 'Display the following summary using a message box
     End Sub
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         'MsgBox to exit form
         Dim Result As DialogResult
+        'Prompt the user with Yes/No buttons in a message box. Ask if they are sure they want to exit.
         Result = MessageBox.Show("Are you sure you want to exit?", "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
-        If Result = DialogResult.Yes Then
+        If Result = DialogResult.Yes Then 'Do not exit unless the user clicks the yes button.
             Me.Close()
         End If
     End Sub
