5645e166922628da3c7c71e63c445f64a0ee176c
Jemika Smith <2cheerbabe3@gmail.com>
2020-04-28 15:17:11 -0600

    created and finished the Etch A Sketch

---
+++ Etch-A-Sketch4_28_20/EtchASketchForm.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EtchASketchForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ChaneColorButton = New System.Windows.Forms.Button()
+        Me.DrawButton = New System.Windows.Forms.Button()
+        Me.EraseButton = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(800, 400)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ChaneColorButton
+        '
+        Me.ChaneColorButton.Location = New System.Drawing.Point(41, 406)
+        Me.ChaneColorButton.Name = "ChaneColorButton"
+        Me.ChaneColorButton.Size = New System.Drawing.Size(115, 45)
+        Me.ChaneColorButton.TabIndex = 1
+        Me.ChaneColorButton.Text = "Change Color"
+        Me.ChaneColorButton.UseVisualStyleBackColor = True
+        '
+        'DrawButton
+        '
+        Me.DrawButton.Location = New System.Drawing.Point(271, 408)
+        Me.DrawButton.Name = "DrawButton"
+        Me.DrawButton.Size = New System.Drawing.Size(115, 41)
+        Me.DrawButton.TabIndex = 2
+        Me.DrawButton.Text = "Draw"
+        Me.DrawButton.UseVisualStyleBackColor = True
+        '
+        'EraseButton
+        '
+        Me.EraseButton.Location = New System.Drawing.Point(624, 406)
+        Me.EraseButton.Name = "EraseButton"
+        Me.EraseButton.Size = New System.Drawing.Size(115, 39)
+        Me.EraseButton.TabIndex = 3
+        Me.EraseButton.Text = "Erase"
+        Me.EraseButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(418, 408)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 4
+        '
+        'EtchASketchForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 477)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.EraseButton)
+        Me.Controls.Add(Me.DrawButton)
+        Me.Controls.Add(Me.ChaneColorButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "EtchASketchForm"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ChaneColorButton As Button
+    Friend WithEvents DrawButton As Button
+    Friend WithEvents EraseButton As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Etch-A-Sketch4_28_20/EtchASketchForm.vb
@@ -0,0 +1,107 @@
+﻿'Jemika Smith
+'RCET0265
+'Spring 2020
+'Etch-A-Sketch
+'https://github.com/smitjemi/JKS-VS-S20
+Public Class EtchASketchForm
+    Dim penColor As Color
+    Dim myPen As New Pen(Color.Black)
+    Dim oldX As Integer
+    Dim oldY As Integer
+    Dim graph As Graphics
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        Me.Text = "x: " & e.X & " y: " & e.Y & " Mouse Button: " & e.Button.ToString
+        graph = PictureBox1.CreateGraphics 'create graphics object
+        If e.Button.ToString <> "None" Then 'draw the line
+            graph.DrawLine(myPen, oldX, oldY, e.X, e.Y)
+        End If
+        oldX = e.X
+        oldY = e.Y
+        'constructor for pen object
+    End Sub
+
+    Private Sub ChaneColorButton_Click(sender As Object, e As EventArgs) Handles ChaneColorButton.Click
+        ColorDialog1.ShowDialog() 'the color wheel 
+        penColor = ColorDialog1.Color 'makes the pen the color the user chose
+        myPen.Color = penColor 'telling the program that the mouse is the pen
+    End Sub
+
+    Private Sub DrawButton_Click(sender As Object, e As EventArgs) Handles DrawButton.Click
+        Dim Xvalue As Decimal
+        Dim Yvalue As Decimal
+        Dim oldY2 = 200
+        Dim oldX2 = 0
+        Label1.Show()
+        myPen.Color = Color.Black
+        graph.Clear(BackColor) 'erase
+        'Verticle Lines (Same X change Y)
+        For xVal = 80 To 720 Step 80
+            For yVal = 5 To 400 Step 10
+                graph.DrawLine(myPen, xVal, yVal, xVal, yVal + 5)
+            Next
+        Next
+        'Horizontal Lines (Same Y change X)
+        For yVal = 40 To 360 Step 40
+            For xVal = 5 To 800 Step 10
+                graph.DrawLine(myPen, xVal, yVal, xVal + 5, yVal)
+            Next
+        Next
+        'Creates sinewave
+        myPen.Color = Color.Blue
+        For X = 1 To 800 'Draw graticules
+            Yvalue = 200 - (50 * Math.Sin(X / 400 * 2 * Math.PI)) 'Draw sine
+            Xvalue = X
+            graph.DrawLine(myPen, oldX2, oldY2, Xvalue, Yvalue)
+            oldX2 = Xvalue
+            oldY2 = Yvalue
+        Next
+        'Creates sinewave phase of cos
+        myPen.Color = Color.Green
+        oldX2 = 0
+        oldY2 = 200
+        For X = 1 To 800 'Draw graticules
+            Yvalue = 200 - (50 * Math.Cos(X / 400 * 2 * Math.PI)) 'Draw Cos
+            Xvalue = X
+            graph.DrawLine(myPen, oldX2, oldY2, Xvalue, Yvalue)
+            oldX2 = Xvalue
+            oldY2 = Yvalue
+        Next
+        'creates a tangent wave
+        myPen.Color = Color.Red
+        oldX2 = 0
+        oldY2 = 200
+        For X = 1 To 800 'Draw graticules
+            Yvalue = 200 - (50 * Math.Tan(X / 400 * 2 * Math.PI)) 'Draw Tan
+            If Yvalue > 600 Then
+                Yvalue = 600
+            ElseIf Yvalue < -200 Then
+                Yvalue = -200
+            End If
+            Xvalue = X
+            If (X > 95 And X < 105) Or (X > 295 And X < 305) Or (X > 495 And X < 505) Or (X > 695 And X < 705) Then
+                ' DO NOT DRAW
+            Else
+                graph.DrawLine(myPen, oldX2, oldY2, Xvalue, Yvalue)
+            End If
+            oldX2 = Xvalue
+            oldY2 = Yvalue
+        Next
+        Label1.Text = ("Sin: Blue" & vbNewLine & "Cos: Green" & vbNewLine & "Tan: Red") 'tells you which colors goes to what waveform.
+    End Sub
+
+    Private Sub EraseButton_Click(sender As Object, e As EventArgs) Handles EraseButton.Click
+        Dim oldTop = Me.Top
+        'Shakes the EtchASketch to erase
+        For i = 1 To 3
+            For j = 1 To 9000000 'Had to shake enough for you to acutally see it move
+                If j < 4500000 Then
+                    Me.Top = oldTop - 50
+                Else
+                    Me.Top = oldTop
+                End If
+            Next
+        Next
+        Label1.Hide() 'shows whichwaveforms are what
+        graph.Clear(BackColor)
+    End Sub
+End Class
